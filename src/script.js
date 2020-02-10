bd47c867aebca91c248e09b076422fb002deb6be
Lisa Bamberg <lisambamberg@gmail.com>
2020-02-10 14:19:17 -0600

    Added Check for Winner Method

--- src/script.js
+++ src/script.js
@@ -19,4 +19,37 @@ $(document).ready(function() {
       currentTurn--;
     }
   });
+
+  function checkForWinner() {
+    if (movesMade > 4) {
+      let moves = Array.prototype.slice.call($(".square"));
+      let results = moves.map(function(square) {
+        return square.innerHTML;
+      });
+
+      let winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      return winningCombinations.find(function(combo) {
+        if (
+          results[combo[0]] !== "" &&
+          results[combo[1]] !== "" &&
+          results[combo[2]] !== "" &&
+          results[combo[0]] === results[combo[1]] &&
+          results[combo[1]] === results[combo[2]]
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+  }
 });
